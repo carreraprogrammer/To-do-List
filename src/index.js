86bb9e6d58b93e12de3284527d54d306f38eb957
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-30 11:12:21 -0500

    add events to add and remove tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 
-
 // Create a ul element 
 const listSection = document.querySelector('section');
 listSection.classList.add('list-section');
@@ -12,47 +11,118 @@ ul.classList.add('display-list');
 
 // Create tasks array 
 
-const tasks = [
-  {
-    description:'Make the bed',
-    completed: true,
-    id: 1
-  },
-  {
-    description:'Study for the exam',
-    completed: false,
-    id: 2
-  },
-  {
-    description:'Do some excersise',
-    completed: false,
-    id: 3
-  },
-  {
-    description:'Do the laundry',
-    completed: false,
-    id: 4
+// The class Task have the methods for select task, add task and remove task from list
+
+class Task {
+
+  constructor(description, id) {
+    this.description = description;
+    this.id = id;
+    this.completed = false;
   }
-]
+
+  static addTask(task) {
+    const tasks = LocalStorage.getTasks();
+    tasks.push(task);
+  }
+
+  static selectTask(select) {
+    return select.addEventListener('click', (e) => {
+        e.target.parentElement.classList.toggle('select');
+        e.target.classList.toggle('ocult');
+        e.target.nextSibling.classList.remove('ocult');
+      });
+  }
+
+  static deleteTask(trash) {
+    return trash.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+        LocalStorage.removeTask(e.target.parentElement.id);
+      });
+  }
+}
+
+// The class LocalStorage have all the methods to save the array information on the local storage
+
+class LocalStorage {
+
+  static getTasks() {
+    let tasks;
+    if(localStorage.getItem('tasks') == null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return tasks;
+  }
+
+  static addTask(task) {
+    const tasks = LocalStorage.getTasks();
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static removeTask(i) {
+    const tasks = LocalStorage.getTasks();
+    const index = tasks.indexOf(tasks[i]);
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+}
 
 // Create display List method
 
 class DisplayList {
 
   static showList() {
-  
+
+    const tasks = LocalStorage.getTasks();
     const displayList = document.querySelector('.display-list');
+    displayList.innerHTML = '';
     const parser = new DOMParser();
     
     tasks.map((task) => {
     const taskString = `
-    <li class='task'><div><input type="checkbox" class='checkbox'>${task.description}</div><i class="fa-solid fa-ellipsis-vertical"></i></li>
+    <li class='task' id=${task.id}><div><input type="checkbox" class='checkbox'>${task.description}</div><i class="fa-solid fa-ellipsis-vertical"></i><i class="fa-regular fa-trash-can ocult"></i></li>
     `
     const taskHtml = parser.parseFromString(taskString, 'text/html').body.firstChild;
     displayList.appendChild(taskHtml);
+
+    const selectBtn = taskHtml.querySelector('.fa-ellipsis-vertical');
+    Task.selectTask(selectBtn);
+    const trashBtn = taskHtml.querySelector('.fa-trash-can');
+    Task.deleteTask(trashBtn);
     })
   
   }
 }
 
 DisplayList.showList();
+
+// Event to create a new task
+
+const addTask = document.getElementById('add-task');
+const submitBtn = document.querySelector('.submit-button');
+
+addTask.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter' && addTask.value > 0) {
+      const tasks = LocalStorage.getTasks();
+      const task = new Task(addTask.value, tasks.length);
+      Task.addTask(task);
+      LocalStorage.addTask(task);
+      DisplayList.showList();
+      addTask.value = '';
+    }
+  }
+)
+
+submitBtn.addEventListener('click', function() {
+  if(addTask.value > 0) {
+    const tasks = LocalStorage.getTasks();
+    const task = new Task(addTask.value, tasks.length);
+    Task.addTask(task);
+    LocalStorage.addTask(task);
+    DisplayList.showList();
+    addTask.value = '';
+  }
+})
